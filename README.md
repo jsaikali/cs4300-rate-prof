ff1cecdc983d8ec8ac5f8e8c3200fc0b8673a9b9
Joanna Saikali <joanna_saikali@harvard.edu>
2021-04-03 20:15:44 -0400

    initial attempt

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # cs4300-rate-prof
+
+## Setup
+
+Install Python 3.9.1 and confirm that the repo is using it:
+
+```bash
+pip install pyenv # or brew install pyenv
+pyenv install 3.9.1
+pyenv local 3.9.1
+eval "$(pyenv init -)"
+pyenv local # Ensure that this outputs 3.9.1
+python --version # Ensure that this outputs Python 3.9.1
+```
+
+[Install Poetry](https://python-poetry.org/docs/), then install the dependencies in a new virtual environment:
+
+```bash
+poetry env use python # Creating virtualenv unrapid in...
+poetry install # Installing dependencies from lock file...
+```
+## Usage
+Execute the script via a command like the following:
+
+```bash
+poetry run python get_rate_prof.py --input data/prof_names.csv --name-col name
+```
+
+This execution is based on an input file `prof_names.csv` where full names of professors at Cornell are in the "name" column. 
+
+This will output a file of ratemyprofessor reviews looking like the following:
+```
+|prof_name|prof_id|prof_dept|class_name|attendance_mandatory|comment|credit|date|difficulty|grade|online_class|rating|take_again|thumbs_down|thumbs_up|
+|--------------|-------|----------------|----------|--------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|------|------------|----------|-----|------------|------|----------|-----------|---------|
+|Tom Avedisian |873434 |Engineering     |MAE324    |                    |One of the least helpful... |FALSE |8/9/06 15:05|3         |     |FALSE       |1     |          |0          |0        |
+|Lorenzo Alvisi|2234527|Computer Science|CS5414    |                    |hws & exams don't accurately...|TRUE  |2/12/21 4:30|5         |F    |FALSE       |1     |FALSE     |0          |1        |
+|Lorenzo Alvisi|2234527|Computer Science|CS5414    |FALSE               |The course content was really interesting ...|TRUE  |1/6/21 8:36 |5         |B-   |FALSE       |3     |TRUE      |0          |0        |
+
+```
+
+
---
+++ get_rate_prof.py
@@ -0,0 +1,63 @@
+'''
+Class for extracting reviews of Cornell professors
+
+Author: Joanna Saikali
+Date:   April 3, 2021
+'''
+import ratemyprofessor
+import pandas as pd
+import datetime
+import argparse
+
+def generate_dataset(input_csv, prof_fullname_col):
+	# Read in file containing prof names of interest
+	# and extract their last name only for querying
+	professor_df = pd.read_csv(input_csv)
+	professor_list = list(professor_df[prof_fullname_col])
+	prof_last_names = [name.split()[-1] for name in professor_list]
+	prof_last_names = list(set(prof_last_names))
+
+	# Get list of Cornell schools
+	# 	Last runtime this produced something like:
+	# 	['Cornell University', 'Cornell College', 'Cornell Law School', ...
+	schools_list = ratemyprofessor.get_schools_by_name("Cornell")
+
+	# Get instances of Professor class for each prof last name at each school
+	all_professors = []
+	for school in schools_list:
+		for prof_name in prof_last_names:
+			prof = ratemyprofessor.get_professors_by_school_and_name(school, prof_name)
+			if len(prof)==20: print("Reached maximum output for name="+prof_name+". May need to refine search")
+			all_professors+=prof
+
+	# Try to filter any duplicate profs
+	all_professors_unique = []
+	[all_professors_unique.append(x) for x in all_professors if x not in all_professors_unique]
+
+	# Get actual ratings of professors
+	records = []
+	for prof in all_professors_unique:
+		ratings = prof.get_ratings()
+		for r in ratings:
+			record = {"prof_name": prof.name, "prof_id":prof.id, "prof_dept":prof.department,
+					"class_name": r.class_name, "attendance_mandatory":r.attendance_mandatory,
+					"comment":r.comment, "credit": r.credit, "date":r.date, "difficulty":r.difficulty,
+					"grade": r.grade, "online_class":r.online_class,"rating":r.rating,"take_again":r.take_again,
+					"thumbs_down":r.thumbs_down, "thumbs_up":r.thumbs_up}
+			records.append(record)
+
+	# Write out ratings dataframe
+	record_df = pd.DataFrame.from_records(records)
+	timestamp = datetime.datetime.now().strftime(format="%Y%m%d%H%M%S")
+	record_df.to_csv("output/rate_my_professor_{}.csv".format(timestamp),index=False)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Create ratemyprofessor dataset')
+    parser.add_argument('--input', dest="input", required=True, help="The CSV file path containing professor names")           
+    parser.add_argument('--name-col', dest="name_col", required=True, help="Column name for professor full names")           
+    args = parser.parse_args()
+
+    generate_dataset(args.input, args.name_col)
+
